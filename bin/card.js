9ae290f21022252958ed636daa7a4fdfa1f172db
Cody Hazelwood <codyhazelwood@users.noreply.github.com>
2018-12-21 18:11:17 -0600

    Add initial version of contact card

---
+++ bin/card.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+console.log(`
+┌─────────────────────────────────────────────────────┐
+│                                                     │ 
+│   Cody Hazelwood | @codyhazelwood                   │
+│   Lead Software Engineer @ FreightWise              │
+│                                                     │
+│   Github    https://github.com/codyhazelwood        │
+│   Twitter   https://twitter.com/codyhazelwood       │
+│   Linkedin  https://linkedin.com/in/codyhazelwood   │
+│   Blog      https://blog.codyhazelwood.me           │
+│                                                     │
+└─────────────────────────────────────────────────────┘
+`);
+
